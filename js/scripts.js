44ce77e5a58b4f4dccbbc4658df2c95068534ddf
jessicakenney <jessicakenney@yahoo.com>
2017-07-07 11:20:35 -0700

    Swap second select Box to a Radio Box to streamline functionality and provide some error-proofing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     alert("hi");
-    var answer_why = $("input:checked").val();
+    var why = $("input:radio[name=why]:checked").val();
+    var frontOrBack = $("input:radio[name=frontOrBack]:checked").val();
+    alert(why);
+    alert(frontOrBack);
 
 
   event.preventDefault();
