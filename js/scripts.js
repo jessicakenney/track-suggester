dd0f5ac89416b25cd654c77c3c997e88bf9c9d30
jessicakenney <jessicakenney@yahoo.com>
2017-07-07 11:30:19 -0700

    Add third question.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
     alert("hi");
     var why = $("input:radio[name=why]:checked").val();
     var frontOrBack = $("input:radio[name=frontOrBack]:checked").val();
+    var size = $("input:radio[name=size]:checked").val();
     alert(why);
     alert(frontOrBack);
+    alert(size);
 
 
   event.preventDefault();
