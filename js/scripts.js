f8cd85868026f1810f34cd881f05dab8c8a8e3a2
jessicakenney <jessicakenney@yahoo.com>
2017-07-07 11:47:57 -0700

    Include all 5 questions to be used as input for branching track suggestions.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
     var why = $("input:radio[name=why]:checked").val();
     var frontOrBack = $("input:radio[name=frontOrBack]:checked").val();
     var size = $("input:radio[name=size]:checked").val();
+    var category = $("input:radio[name=category]:checked").val();
+    var platform = $("input:radio[name=platform]:checked").val();
     alert(why);
     alert(frontOrBack);
     alert(size);
-
+    alert(platform);
 
   event.preventDefault();
   });
