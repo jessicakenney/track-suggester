3c9b25d1882f6ce1e4be9e9fbe001b48fb3d35fa
jessicakenney <jessicakenney@yahoo.com>
2017-07-07 12:34:27 -0700

    Early functionality of suggestion functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,31 @@ $(document).ready(function() {
     var size = $("input:radio[name=size]:checked").val();
     var category = $("input:radio[name=category]:checked").val();
     var platform = $("input:radio[name=platform]:checked").val();
-    alert(why);
-    alert(frontOrBack);
-    alert(size);
-    alert(platform);
+    // debug
+    // alert(why);
+    // alert(frontOrBack);
+    // alert(size);
+    // alert(platform);
+
+    $(".why").text(why);
+    $(".frontOrBack").text(frontOrBack);
+    $("#summary").show();
+    alert("calculating your best fit");
+    // need to put branching here to calculatedefin suggestion
+    var suggestRuby = 0;
+    var suggestJava = 1;
+    var suggestCSS = 0;
+    if (suggestRuby) {
+      $(".suggestRuby").show();
+    } else if (suggestJava) {
+      $(".suggestJava").show();
+    } else if (suggestCSS) {
+      $(".suggestCSS").show();
+    }
+
+
+
+
 
   event.preventDefault();
   });
